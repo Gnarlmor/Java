8610ded3dd051386c5ccc02f79308477df52d0da
Gnarlmor <grigore_gherman@icloud.com>
2025-02-28 23:49:35 +0200

    I/O Implementation

--- src/laborator/Main.java
+++ src/laborator/Main.java
@@ -1,7 +1,13 @@
 package laborator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a=");
+        int a = scanner.nextInt();
+        System.out.println("Ati introdus valoarea " + a);
+        scanner.close();
     }
 }
